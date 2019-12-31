fd90c7f3654e050f85c523b8cf6620bc5ab158b8
Nick Kapetanas <nkapetanas@outlook.com>
2019-12-31 15:24:01 +0200

    added kfold implementation, metrics - pending mean calculation of metrics

--- imdbTextClassificationBagOfWords.py
+++ imdbTextClassificationBagOfWords.py
@@ -17,9 +17,9 @@ from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, cross_val_predict
 from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVR
 
 DATASET_PATH_TRAIN = "C:/Users/Delta/PycharmProjects/Sentiment-Analysis/dataset/train.csv"
 DATASET_PATH_TEST = "C:/Users/Delta/PycharmProjects/Sentiment-Analysis/dataset/test_without_labels.csv"
@@ -59,13 +59,15 @@ def clean_data(dataframe):
     dataframe['Content'] = dataframe['Content'].apply(
         lambda x: ' '.join([stemmer.stem(word) for word in x.split()]))
 
+
 def calculate_metrics(y_actual, y_predicted):
     accuracy = accuracy_score(y_actual, y_predicted)
-    precision = precision_score(y_actual, y_predicted, average='micro')
-    recall = recall_score(y_actual, y_predicted, average='micro')
-    f1 = f1_score(y_actual, y_predicted, average='micro')
+    precision = precision_score(y_actual, y_predicted)
+    recall = recall_score(y_actual, y_predicted)
+    f1 = f1_score(y_actual, y_predicted)
 
     return accuracy, precision, recall, f1
+
 
 train_data = read_dataset(DATASET_PATH_TRAIN)
 test_data = read_dataset(DATASET_PATH_TEST)
@@ -75,20 +77,30 @@ clean_data(test_data)
 
 x_train_data = train_data["Content"]
 y_train_data = train_data["Label"]
-
-x_train, x_test, y_train, y_test = train_test_split(x_train_data, y_train_data, test_size=0.3, shuffle=True)
-
-ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 3))
-X_train = ngram_vectorizer.fit_transform(x_train)
-X_test = ngram_vectorizer.transform(x_test)
-
+classes = np.unique(train_data['Label'])
 
 kfold = KFold(n_splits=5, random_state=42, shuffle=True)
 
 fold = 0
+for train_index, test_index in kfold.split(x_train_data):
+    fold += 1
+    print("Fold: %s" % fold)
 
-sgd_classifier = SGDClassifier(max_iter=20)
-sgd_classifier.fit(X_train, y_train)
+    x_train_k, x_test_k = x_train_data.iloc[train_index], x_train_data.iloc[test_index]
+    y_train_k, y_test_k = y_train_data.iloc[train_index], y_train_data.iloc[test_index]
 
-print("Accuracy SGDClassifier: %s"
-      % (accuracy_score(y_test, sgd_classifier.predict(X_test))))
+    ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 3))
+    X_train = ngram_vectorizer.fit_transform(x_train_k)
+    X_test = ngram_vectorizer.transform(x_test_k)
+
+    sgd_classifier = SGDClassifier(max_iter=1000, loss='hinge')
+    sgd_classifier.fit(X_train, y_train_k)
+    predictedValues = sgd_classifier.predict(X_test)
+
+    print("Accuracy SGDClassifier: %s"
+          % (accuracy_score(y_test_k, predictedValues)))
+    accuracy, precision, recall, f1 = metrics = calculate_metrics(y_test_k, predictedValues)
+    print("accuracy:" + str(accuracy))
+    print("precision:" + str(precision))
+    print("recall:" + str(recall))
+    print("f1:" + str(f1))
