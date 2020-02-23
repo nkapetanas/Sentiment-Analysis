1c5f10299bc9a2d8507e8727cc0e432bd8127018
Nick Kapetanas <nkapetanas@outlook.com>
2020-02-23 20:37:10 +0200

    added plot generator

--- imdbTextClassificationBagOfWords.py
+++ imdbTextClassificationBagOfWords.py
@@ -1,14 +1,14 @@
-import string
-
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, plot_precision_recall_curve
-from sklearn.model_selection import KFold, validation_curve
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import KFold
+
+from plot_learning_curve import plot_learning_curve
 
 DATASET_PATH_TRAIN = "C:/Users/Delta/PycharmProjects/Sentiment-Analysis/dataset/train.csv"
 DATASET_PATH_TEST = "C:/Users/Delta/PycharmProjects/Sentiment-Analysis/dataset/test_without_labels.csv"
@@ -17,6 +17,10 @@ DATASET_PATH_TEST = "C:/Users/Delta/PycharmProjects/Sentiment-Analysis/dataset/t
 TOKENS_ALPHANUMERIC = '[A-Za-z0-9]+(?=\\s+)'
 stop = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
+
+
+def plotPrecisionRecallCurve(classifier, x_, y_):
+    plot_learning_curve(classifier, 'Learning Curve', x_, y_, cv=5)
 
 
 def createCSV(prediction, csvName):
@@ -29,14 +33,13 @@ def read_dataset(dataset):
     df = pd.read_csv(dataset, encoding='utf-8')
     return df
 
+
 def clean_data(dataframe):
     dataframe['Content'] = dataframe['Content'].str.lower()
     dataframe['Content'] = dataframe['Content'].str.replace('[^\w\s]', '')
     dataframe['Content'] = dataframe['Content'].str.replace('<[^<]+?>', '')  # remove HTML tags
     dataframe['Content'] = dataframe['Content'].apply(
         lambda x: ' '.join([item for item in x.split() if item not in stop]))
-    # dataframe['Content'] = dataframe['Content'].apply(
-    #     lambda x: ' '.join([lemmatizer.lemmatize(word) for word in x.split()]))
 
 
 def calculate_metrics(y_actual, y_predicted):
@@ -91,6 +94,10 @@ for train_index, test_index in kfold.split(x_train_data):
     scores_svm_recall.append(recall)
     scores_svm_f1.append(f1)
 
+input_data_to_plot = ngram_vectorizer.transform(x_train_data)
+plotPrecisionRecallCurve(sgd_classifier, input_data_to_plot, y_train_data)
+plt.show()
+
 print("SGDClassifier metrics")
 print("Accuracy:" + str(np.mean(scores_svm_accuracy)))
 print("Precision:" + str(np.mean(scores_svm_precision)))
@@ -101,7 +108,3 @@ test_data_ = ngram_vectorizer.transform(test_data_)
 
 predictedValues = sgd_classifier.predict(test_data_)
 createCSV(predictedValues, "sentiment_predictions.csv")
-
-# X_train = ngram_vectorizer.fit_transform(x_train_data)
-# plot_precision_recall_curve(sgd_classifier, X_train, y_train_data)
-# plt.show()
